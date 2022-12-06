92afef0c60569c06db076867a08c28815fd02489
Ali Mortazavi <mortazavimaneshali@gmail.com>
2022-12-06 11:36:28 +0330

    FB

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://beige-woodpecker-cuff.cyclic.app/api/v1",
+  baseURL: "https://beige-woodpecker-cuff.cyclic.app/api/v1", 
 });
 
 export default api;
