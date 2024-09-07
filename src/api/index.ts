3fa5fbefe361ce63efba2e5ad150faf82a09fad8
Ali Mortazavi <mortazavimaneshali@gmail.com>
2024-09-08 01:12:48 +0330

    change url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ptasks-back.cyclic.app/api/v1",
+  baseURL: "https://ptasks.liara.run/api/v1",
 });
 
 export default api;
